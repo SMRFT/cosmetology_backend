b12c013c66ce0ec147a4951c257ec9a3f6d5b5a2
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-06-12 17:58:08 +0530

    Update settings-test.py

--- cosmo_backend/settings-test.py
+++ cosmo_backend/settings-test.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000","http://localhost:3001"
+    "http://localhost:3000","http://localhost:3001","https://testcosmetology.shinova.in"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
